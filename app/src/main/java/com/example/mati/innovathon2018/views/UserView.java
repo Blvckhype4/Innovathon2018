da68111e7aac72fe621a2b3c3c1d5ce9a82d5f84
returnv01d <bartlomiej.winiarski88@gmail.com>
2018-04-21 00:31:32 +0200

    created user view

--- app/src/main/java/com/example/mati/innovathon2018/views/UserView.java
+++ app/src/main/java/com/example/mati/innovathon2018/views/UserView.java
@@ -1,4 +1,24 @@
 package com.example.mati.innovathon2018.views;
 
-public class UserView {
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class UserView extends View {
+    private Rect mBounds;
+    private Context mContext;
+
+    public UserView(Context context, AttributeSet attrs, Rect mBounds) {
+        super(context, attrs);
+        mContext = context;
+        this.mBounds = mBounds;
+
+    }
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+    }
 }
