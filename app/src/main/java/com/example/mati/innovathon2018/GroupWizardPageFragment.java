0dfcc982d5aef862afbbedf5896b8ed2b32dda05
Adam Borys <hipowolemia@gmail.com>
2018-04-21 14:33:57 +0200

    Pracuję nad wizardem grupy

---
+++ app/src/main/java/com/example/mati/innovathon2018/GroupWizardPageFragment.java
@@ -0,0 +1,19 @@
+package com.example.mati.innovathon2018;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class GroupWizardPageFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.fragment_group_wizard, container, false);
+
+        return rootView;
+    }
+}
