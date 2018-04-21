5dbf9cf7ee3844d58453bbe7689ea0d0aa8503fc
Blvck <mati12899@o2.pl>
2018-04-21 13:55:27 +0200

    Remake of login

--- app/src/main/java/com/example/mati/innovathon2018/MainActivity.java
+++ app/src/main/java/com/example/mati/innovathon2018/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.mati.innovathon2018;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.annotation.NonNull;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +13,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private TextView mFullName, mEmail;
+    private ImageView mAvatar;
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+    private FirebaseAuth.AuthStateListener mListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +35,6 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -40,6 +44,32 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+
+
+        mListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                    if (firebaseAuth.getCurrentUser() != null) {
+                    }
+                }
+        };
+        mFullName = (TextView) findViewById(R.id.fullName);
+        mEmail = (TextView) findViewById(R.id.email);
+        mAvatar = (ImageView) findViewById(R.id.avatar);
+
+        View headerView = navigationView.getHeaderView(0);
+        mFullName = (TextView) headerView.findViewById(R.id.fullName);
+        mFullName.setText(mUser.getDisplayName().toString());
+
+        mEmail = (TextView) headerView.findViewById(R.id.email);
+        mEmail.setText(mUser.getEmail().toString());
+
+        mAvatar = (ImageView) headerView.findViewById(R.id.avatar);
+        Uri uri = Uri.parse(mUser.getPhotoUrl().toString());
+        Picasso.with(MainActivity.this).load(uri).resize(200,200).into(mAvatar);
     }
 
     @Override
@@ -80,22 +110,35 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.searchGroup) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.createGroup) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.searchProfile) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.settings) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.signOut) {
+            mAuth.signOut();
+            startActivity(new Intent(MainActivity.this, ChooseLoginMethod.class));
+            finish();
 
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mAuth.removeAuthStateListener(mListener);
     }
 }
