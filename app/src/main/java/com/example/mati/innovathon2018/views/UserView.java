ad05c253059e74aab27afcbe38ec881a07e19708
returnv01d <bartlomiej.winiarski88@gmail.com>
2018-04-21 04:10:40 +0200

    created user view layout

--- app/src/main/java/com/example/mati/innovathon2018/views/UserView.java
+++ app/src/main/java/com/example/mati/innovathon2018/views/UserView.java
@@ -2,18 +2,17 @@ package com.example.mati.innovathon2018.views;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class UserView extends View {
-    private Rect mBounds;
+   
     private Context mContext;
 
-    public UserView(Context context, AttributeSet attrs, Rect mBounds) {
+    public UserView(Context context, AttributeSet attrs) {
         super(context, attrs);
         mContext = context;
-        this.mBounds = mBounds;
+
 
     }
     @Override
